c036c7fe900c24473392e05acbbe591fbcb0f11c
ABHINAV KUMAR <abhikumar3584@gmail.com>
2026-01-07 19:02:22 +0530

    Add non-division exam data structure and remove pricing table from home page

--- pariksha_yogya/src/Pages/home.jsx
+++ pariksha_yogya/src/Pages/home.jsx
@@ -2,7 +2,6 @@ import { Hero } from "@/components/Hero/hero";
 import InfinityImgScroll from "@/components/Infinityimgscroll/infinityimgscroll";
 // import FeatureSection from "@/components/Featurecards/cardscontent";
 import InfinityExams from "@/components/Infinityexams/infinityexams";
-import { PricingTableDemo } from "@/components/Payment/payment";
 import FAQS from "@/components/FAQS/faqs";
 import Footer from "@/components/Footer/footer";
 import { GlowingEffectDemo } from "../components/Featurecards/cardscontent";
@@ -29,10 +28,6 @@ function Home() {
 
       <div className="w-full mx-0 px-0">
         <InfinityExams />
-      </div>
-
-      <div className="w-full p-4 pb-0 m-auto">
-        <PricingTableDemo />
       </div>
 
       <div className="w-full p-4 pb-0 pt-0 m-auto">
